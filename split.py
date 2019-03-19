e6c000c00a84376f32ac8f4b26a287fb48c32042
jaeeunc1 <33257487+jaeeunc1@users.noreply.github.com>
2019-03-18 23:49:36 -0400

    Update split.py

--- split.py
+++ split.py
@@ -7,19 +7,19 @@ length = len(os.listdir(path))
 
 os.makedirs("./awsfish/validation")
 os.makedirs("./awsfish/train")
-os.makedirs("./awsfish/test")
+#os.makedirs("./awsfish/test")
 
-train = length * 0.6
-validation = length * 0.2
-test = length - train - validation
+train = length * 0.8
+validation = length - train
+#test = length - train - validation
 i = 1
 
 for filename in os.listdir(path):
     print(filename)
     if i <= train:
         shutil.move(path + filename, "./awsfish/train/" + filename)
-    if i > train and i <= validation + train:
-        shutil.move(path + filename, "./awsfish/validation/" + filename)
     else:
-        shutil.move(path + filename, "./awsfish/test/" + filename)
+        shutil.move(path + filename, "./awsfish/validation/" + filename)
+    #else:
+        #shutil.move(path + filename, "./awsfish/test/" + filename)
     i +=1
